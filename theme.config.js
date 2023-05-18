50640e334d8c08b30af4d42f844afb9b2d2a3498
Cliff Mirschel <cliff.mirschel@gmail.com>
2023-05-17 17:01:40 -0700

    yee haw!!

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,33 @@
 const YEAR = new Date().getFullYear()
+import { FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { DiGithubAlt } from 'react-icons/di';
+import copy from "copy-to-clipboard";
 
+function copy_text() {
+  copy("cliffordmirschel@gmail.com");
+}
 export default {
-  footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Cliff Mirschel.
-      <a href="https://www.linkedin.com/in/cliff-mirschel/" target="_blank">linkedin</a>
-      <style jsx>{`
+
+  footer:
+    <div className='footer'>
+      <small style={{ display: 'block', marginTop: '8rem' }}>
+        <time>{YEAR}</time> © Cliff Mirschel.
+
+
+        <a href="https://github.com/BlueBoi904" target="_blank"><DiGithubAlt /></a>
+
+        <a href="https://www.linkedin.com/in/cliff-mirschel/" target="_blank"><FaLinkedin /></a>
+
+        <a href="https://mail.google.com/mail" onclick={copy_text} target="_blank"><FaEnvelope /></a>
+        <style jsx>{`
         a {
+          float: right;
+          padding-right: .25rem;
+          padding-left: .25rem;
+        }
+        .footer{
+          display: flex;
+          align-items: center;
           float: right;
         }
         @media screen and (max-width: 480px) {
@@ -16,6 +37,10 @@ export default {
           }
         }
       `}</style>
-    </small>
-  )
+      </small>
+    </div>
+
 }
+//  - GitHub: [blueboi904](https://github.com/blueboi904) <DiGithubAlt />
+//  - Linkedin: [in/cliff-mirschel](https://www.linkedin.com/in/cliff-mirschel/) <FaLinkedin />
+//  - Email: cliffordmirschel@gmail.com <FaEnvelope />
